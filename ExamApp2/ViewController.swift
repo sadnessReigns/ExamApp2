356255280fa1b7ac14b6b2f798cb06165df8a7e3
Vladislav McKay <from.pandaren@gmail.com>
2020-07-22 10:54:56 +0300

    Initial Commit

---
+++ ExamApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExamApp2
+//
+//  Created by Vladislav McKay on 7/22/20.
+//  Copyright © 2020 Vladislav McKay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
